97894b1ca2f25f38c090fc5afc1114121d19c029
Paul Berry <paul@puppetlabs.com>
2011-01-29 11:32:05 -0800

    Used registry to render the "compare with" widget on the report view page.

--- vendor/plugins/baseline/init.rb
+++ vendor/plugins/baseline/init.rb
@@ -1,1 +1,6 @@
 # Include hook code here
+Registry.add_hook :core, :report_view_widgets, "500_baseline", lambda { |view_renderer, report|
+  if report.kind == "inspect" and Report.baselines.any?
+    "<div class='section'>#{view_renderer.render 'baseline_selector', :diffee => report, :action => 'diff'}</div>"
+  end
+}
