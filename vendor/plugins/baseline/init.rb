4e3715b9a7c90ae313e069c6d26c1b864c7e1219
Paul Berry <paul@puppetlabs.com>
2011-01-29 11:40:32 -0800

    Used hook for baesline selector in node group display

--- vendor/plugins/baseline/init.rb
+++ vendor/plugins/baseline/init.rb
@@ -1,6 +1,10 @@
-# Include hook code here
 Registry.add_hook :core, :report_view_widgets, "500_baseline", lambda { |view_renderer, report|
   if report.kind == "inspect" and Report.baselines.any?
     "<div class='section'>#{view_renderer.render 'baseline_selector', :diffee => report, :action => 'diff'}</div>"
+  end
+}
+Registry.add_hook :core, :node_group_view_widgets, "500_baseline", lambda { |view_renderer, node_group|
+  if Report.baselines.any?
+    view_renderer.render 'reports/baseline_selector', :diffee => node_group, :action => 'diff_group'
   end
 }
