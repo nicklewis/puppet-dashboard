b754c5bcd26a61ff34a9dd182371f5e3bbe18139
Paul Berry <paul@puppetlabs.com>
2011-01-29 11:15:59 -0800

    Moved node group diff into the plugin

--- vendor/plugins/baseline/config/routes.rb
+++ vendor/plugins/baseline/config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
   map.diffs "reports/:id/diff", {:controller=>"diffs", :action=>"diff"}
+  map.diff_group "node_groups/:id/diff", {:controller=>"diffs", :action=>"diff_group"}
 end
 
