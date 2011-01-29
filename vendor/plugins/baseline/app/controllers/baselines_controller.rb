8f3e6660a4b32b362f1ed3cc83cef17c0a47f3eb
Paul Berry <paul@puppetlabs.com>
2011-01-29 12:40:55 -0800

    Moved the "baselines" controller action (used for autocomplete) into the plugin

--- vendor/plugins/baseline/app/controllers/baselines_controller.rb
+++ vendor/plugins/baseline/app/controllers/baselines_controller.rb
@@ -4,4 +4,17 @@ class BaselinesController < ApplicationController
     report.baseline!
     redirect_to report
   end
+
+  def index
+    if request.format == :json
+      limit = params[:limit].to_i
+      search_term = params[:term].gsub(/([\\%_])/, "\\\\\\1")
+      prefix_matches = Report.baselines.where(["host LIKE ?", "#{search_term}%"]).order("host ASC").limit(limit).map(&:host)
+      substring_matches = Report.baselines.where(["host LIKE ?", "%#{search_term}%"]).order("host ASC").limit(limit).map(&:host)
+      matches = (prefix_matches + substring_matches).uniq[0,limit]
+      render :text => matches.to_json, :content_type => 'application/json'
+    else
+      render :status => 406
+    end
+  end
 end
--- vendor/plugins/baseline/config/routes.rb
+++ vendor/plugins/baseline/config/routes.rb
@@ -2,5 +2,6 @@ ActionController::Routing::Routes.draw do |map|
   map.diffs "reports/:id/diff", {:controller=>"diffs", :action=>"diff"}
   map.diff_group "node_groups/:id/diff", {:controller=>"diffs", :action=>"diff_group"}
   map.make_baseline "reports/:id/make_baseline", {:controller=>"baselines", :action=>"make_baseline"}
+  map.baselines "reports/baselines", {:controller=>"baselines", :action=>"index"}
 end
 
