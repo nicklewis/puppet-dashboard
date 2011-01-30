fde360f0c050be3c46236d7e939040a6df6aa4aa
Paul Berry <paul@puppetlabs.com>
2011-01-29 17:04:19 -0800

    Removed the old schema for baselines.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110129205337) do
+ActiveRecord::Schema.define(:version => 20110130010154) do
 
   create_table "baselines", :force => true do |t|
     t.integer "node_id"
@@ -75,7 +75,6 @@ ActiveRecord::Schema.define(:version => 20110129205337) do
     t.integer  "last_apply_report_id"
     t.string   "status"
     t.boolean  "hidden",                 :default => false
-    t.integer  "baseline_report_id"
     t.integer  "last_inspect_report_id"
   end
 
